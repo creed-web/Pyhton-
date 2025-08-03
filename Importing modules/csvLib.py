b291f6194dd7a8ade48eaa0327c8f3861b0e2f11
creed-web <varunkumarsaxena26@gmail.com>
2025-08-03 13:12:38 +0530

    Learned and explored the csv library

---
+++ Importing modules/csvLib.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('example.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['name','age'])
+    writer.writerow(['varun', '19'])
+
+with open ('example.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
