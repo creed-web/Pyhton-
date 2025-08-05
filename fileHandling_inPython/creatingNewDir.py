cc2db5af007cbd4ef4b7de2d6d9745033d9c74af
creed-web <varunkumarsaxena26@gmail.com>
2025-08-05 14:02:34 +0530

    learned the list function

--- fileHandling_inPython/creatingNewDir.py
+++ fileHandling_inPython/creatingNewDir.py
@@ -1,5 +1,9 @@
 import os
 
-new_directory = "package"
+'''new_directory = "package"
 os.mkdir(new_directory)
-print(f"Directory {new_directory} created")
+print(f"Directory {new_directory} created")'''
+
+#listing files and directories 
+items = os.listdir('.')
+print(items)
