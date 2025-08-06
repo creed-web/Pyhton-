a1575d11e8249dda56b8adfaa2457e9c6c9e89d6
creed-web <varunkumarsaxena26@gmail.com>
2025-08-06 23:14:50 +0530

    Learned the exception class in python

---
+++ exceptionHandling/ExceptionClass.py
@@ -0,0 +1,5 @@
+#This exception keyword here is exception class in Python which is used to print any type of exception we have this exception class contains all kind of exceptions
+try:
+    result = 1/0
+except Exception as ex:
+    print(ex)
