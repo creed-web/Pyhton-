249b6d5b3f45dde987775a4d1c840e679bc70d76
creed-web <varunkumarsaxena26@gmail.com>
2025-08-07 18:15:31 +0530

    Started learning OOP's in python

---
+++ OOP's/creatingClasses.py
@@ -0,0 +1,10 @@
+class varun:
+    pass #here pass means i'm not writing any logic here for this class
+
+varun.eyes = 2
+varun.legs = 2
+varun.brain = 1
+
+print(type(varun.legs))
+
+print(dir(varun))
