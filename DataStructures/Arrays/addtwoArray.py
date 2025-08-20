39a466787e78f046d7e65436b2e2dd6f11d5b69d
creed-web <varunkumarsaxena26@gmail.com>
2025-08-21 01:31:19 +0530

    #WAP to add two arrays

--- DataStructures/Arrays/addtwoArray.py
+++ DataStructures/Arrays/addtwoArray.py
@@ -1,9 +1,13 @@
 #WAP to add two arrays
 
-arr1 = [10,20,30,40,50]
-arr2 = [1,2,3,4,5]
+arr1 = [3,4,5,6,7,8]
+arr2 = [0,9,8,7,6,5]
 addedArr = []
+n = 6
+
 for i in range(len(arr1)):
-    addedArr[i] = arr1[i]+arr2[i]
+    addedArr.append(arr1[i] + arr2[i])
+
+for i in range(len(addedArr)):
+    print(addedArr[i], end= ' ')
     
-print(addedArr)
